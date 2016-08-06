6687beae4900920afe104dccca3f2430a65c073e
ramadhinolfski <hanafiah.ramadhan@gmail.com>
2016-08-06 23:02:41 +0700

    Set up default configuration

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
-
+$http = "http" . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? "s" : "") . "://" ;
+$url = str_replace("index.php", "", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $http . $_SERVER['SERVER_NAME'] . $url;
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -35,7 +36,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
