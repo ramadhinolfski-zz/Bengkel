500e0728163a253f5424ca3611abfbfc1ed34d66
ramadhinolfski <hanafiah.ramadhan@gmail.com>
2016-08-06 23:39:20 +0700

    Add css link

---
+++ application/views/layout/css.php
@@ -0,0 +1,10 @@
+<!-- Bootstrap -->
+<link href="<?php echo base_url();?>/theme/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Font Awesome -->
+<link href="<?php echo base_url();?>/theme/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<!-- NProgress -->
+<link href="<?php echo base_url();?>/theme/nprogress/nprogress.css" rel="stylesheet">
+<!-- Animate.css -->
+<link href="<?php echo base_url();?>/theme/animate.css/animate.css" rel="stylesheet">
+<!-- Custom Theme Style -->
+<link href="<?php echo base_url();?>/theme/css/custom.min.css" rel="stylesheet">
