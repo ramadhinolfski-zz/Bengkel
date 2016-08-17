6865f447a3088c92f34e57a0d95549d03c493a95
ramadhinolfski <hanafiah.ramadhan@gmail.com>
2016-08-17 20:49:17 +0700

    Add some datatables

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -16,4 +16,12 @@
 
 <!-- Custom Theme Style -->
 <link href="<?php echo base_url();?>theme/css/custom.min.css" rel="stylesheet">
+
+<!-- Datatable -->
+<link href="<?php echo base_url(); ?>theme/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>theme/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>theme/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>theme/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
+<link href="<?php echo base_url(); ?>theme/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
+    
 </head>
